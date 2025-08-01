8ea41aacc2bc20c2574a8d56f2eca0299266e73d
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 16:54:06 +0300

    Add files via upload

---
+++ uygulama-15/uygulama-15/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(progressBar1.Value<=90)
+            progressBar1.Value += 10;
+            else
+            {
+                MessageBox.Show("daha fazla tıklıyamazsınız");
+            }
+        }
+    }
+}
