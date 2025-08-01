9277ad8fbd40ac95b52448f0e5001be5c1943140
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 16:28:53 +0300

    Add files via upload

---
+++ uygulama-13/uygulama-13/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+            int toplam = 0;
+            for(int i=0;  i <= sayi; i++){
+                toplam += i;
+
+            }
+            label1.Text = toplam.ToString();
+        }
+    }
+}
