3eba6088a1dccc739d6a79fadd0ba466583b6610
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 17:07:26 +0300

    Add files via upload

---
+++ uygulama-16/uygulama-16/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama_16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value <= 90)
+            {
+                progressBar1.Value += 10;
+            }
+
+
+        }
+    }
+}
