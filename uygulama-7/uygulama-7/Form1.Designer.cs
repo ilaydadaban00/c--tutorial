e77fb64ce6d32b95714819866ef5b4ef7d725b17
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 14:47:52 +0300

    Add files via upload

---
+++ uygulama-7/uygulama-7/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace uygulama_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnHesap = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.txtKisakenar = new System.Windows.Forms.TextBox();
+            this.txtUzunkenar = new System.Windows.Forms.TextBox();
+            this.txtYaricap = new System.Windows.Forms.TextBox();
+            this.lblKare = new System.Windows.Forms.Label();
+            this.lblDikdortgen = new System.Windows.Forms.Label();
+            this.lblYaricap = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnHesap
+            // 
+            this.btnHesap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnHesap.Location = new System.Drawing.Point(93, 388);
+            this.btnHesap.Name = "btnHesap";
+            this.btnHesap.Size = new System.Drawing.Size(570, 50);
+            this.btnHesap.TabIndex = 1;
+            this.btnHesap.Text = "Hesap";
+            this.btnHesap.UseVisualStyleBackColor = false;
+            this.btnHesap.Click += new System.EventHandler(this.btnHesap_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 172);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(215, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Geometri Calculator";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.lblKare);
+            this.panel2.Controls.Add(this.txtM);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(3, 174);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(244, 180);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel3.Controls.Add(this.lblDikdortgen);
+            this.panel3.Controls.Add(this.txtUzunkenar);
+            this.panel3.Controls.Add(this.txtKisakenar);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Location = new System.Drawing.Point(243, 174);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(278, 180);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.lblYaricap);
+            this.panel4.Controls.Add(this.txtYaricap);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.pictureBox3);
+            this.panel4.Location = new System.Drawing.Point(522, 174);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(278, 180);
+            this.panel4.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(44, -14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(27, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(180, 76);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(79, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(91, 80);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kenar(m) :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kısa kenar:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Uzun Kenar:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Yarıçap:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtM
+            // 
+            this.txtM.Location = new System.Drawing.Point(90, 112);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(100, 20);
+            this.txtM.TabIndex = 2;
+            // 
+            // txtKisakenar
+            // 
+            this.txtKisakenar.Location = new System.Drawing.Point(90, 84);
+            this.txtKisakenar.Name = "txtKisakenar";
+            this.txtKisakenar.Size = new System.Drawing.Size(100, 20);
+            this.txtKisakenar.TabIndex = 3;
+            // 
+            // txtUzunkenar
+            // 
+            this.txtUzunkenar.Location = new System.Drawing.Point(90, 112);
+            this.txtUzunkenar.Name = "txtUzunkenar";
+            this.txtUzunkenar.Size = new System.Drawing.Size(100, 20);
+            this.txtUzunkenar.TabIndex = 4;
+            // 
+            // txtYaricap
+            // 
+            this.txtYaricap.Location = new System.Drawing.Point(81, 96);
+            this.txtYaricap.Name = "txtYaricap";
+            this.txtYaricap.Size = new System.Drawing.Size(100, 20);
+            this.txtYaricap.TabIndex = 6;
+            // 
+            // lblKare
+            // 
+            this.lblKare.AutoSize = true;
+            this.lblKare.Location = new System.Drawing.Point(107, 150);
+            this.lblKare.Name = "lblKare";
+            this.lblKare.Size = new System.Drawing.Size(13, 13);
+            this.lblKare.TabIndex = 3;
+            this.lblKare.Text = "0";
+            // 
+            // lblDikdortgen
+            // 
+            this.lblDikdortgen.AutoSize = true;
+            this.lblDikdortgen.Location = new System.Drawing.Point(128, 150);
+            this.lblDikdortgen.Name = "lblDikdortgen";
+            this.lblDikdortgen.Size = new System.Drawing.Size(13, 13);
+            this.lblDikdortgen.TabIndex = 4;
+            this.lblDikdortgen.Text = "0";
+            // 
+            // lblYaricap
+            // 
+            this.lblYaricap.AutoSize = true;
+            this.lblYaricap.Location = new System.Drawing.Point(111, 150);
+            this.lblYaricap.Name = "lblYaricap";
+            this.lblYaricap.Size = new System.Drawing.Size(13, 13);
+            this.lblYaricap.TabIndex = 5;
+            this.lblYaricap.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnHesap);
+            this.Name = "Form1";
+            this.Text = "Geometri hesaplıyıcı";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnHesap;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.TextBox txtUzunkenar;
+        private System.Windows.Forms.TextBox txtKisakenar;
+        private System.Windows.Forms.TextBox txtYaricap;
+        private System.Windows.Forms.Label lblKare;
+        private System.Windows.Forms.Label lblDikdortgen;
+        private System.Windows.Forms.Label lblYaricap;
+    }
+}
+
---
+++ uygulama-7/uygulama-7/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHesap_Click(object sender, EventArgs e)
+        {
+            int m = Convert.ToInt32(txtM.Text);
+            int sonucKare = m * m ;
+            lblKare.Text =Convert.ToString("karenin Alanı  "+sonucKare);
+
+
+
+
+
+
+
+            int k = Convert.ToInt32(txtKisakenar.Text);
+            int u = Convert.ToInt32(txtUzunkenar.Text);
+            int sonucDikdortgen = k * u;
+            lblDikdortgen.Text = Convert.ToString("dikdörtgenin alanı" + sonucDikdortgen);
+
+
+
+            int yaricap = Convert.ToInt32(txtYaricap.Text);
+            int sonucdaire = (int)Math.PI *(yaricap* yaricap);
+            lblYaricap.Text = Convert.ToString("dairenin  Alanı" + sonucdaire);
+
+
+        }
+    }
+}
