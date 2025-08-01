21d5d241e36601ac40427754f3c31442553d2ab1
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 15:02:28 +0300

    Add files via upload

---
+++ uygulama-8/uygulama-8/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Toplama()
+        {
+
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+
+            int sonuc = sayi1 + sayi2;
+            lblSonuc.Text = sonuc.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Toplama();
+        }
+    }
+}
