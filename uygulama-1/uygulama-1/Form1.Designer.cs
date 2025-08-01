a0dd71153f3e68393083c211148b4767f301093f
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 12:14:18 +0300

    Add files via upload

---
+++ uygulama-1/uygulama-1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace uygulama_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSonuc = new System.Windows.Forms.Button();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSonuc
+            // 
+            this.btnSonuc.Location = new System.Drawing.Point(214, 200);
+            this.btnSonuc.Name = "btnSonuc";
+            this.btnSonuc.Size = new System.Drawing.Size(75, 23);
+            this.btnSonuc.TabIndex = 0;
+            this.btnSonuc.Text = "button1";
+            this.btnSonuc.UseVisualStyleBackColor = true;
+            this.btnSonuc.Click += new System.EventHandler(this.btnSonuc_Click);
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(335, 202);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 1;
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(335, 176);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.btnSonuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSonuc;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.TextBox txtSayi1;
+    }
+}
+
