d5d052fb99039ae97a487763cc9120f5352ddba9
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2025-08-01 16:45:53 +0300

    Add files via upload

---
+++ uygulama-14/uygulama-14/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            int baslangic = Convert.ToInt32(textBox1.Text);
+            int bittis = Convert.ToInt32(textBox2.Text);
+
+
+            for (int i = baslangic; i < bittis; i++)
+            {
+                if (i % 2 == 0)
+                
+                    richTextBox1.Text = richTextBox1.Text + i + " ";
+                
+            }
+            
+            
+                
+            
+
+        }
+    }
+}
